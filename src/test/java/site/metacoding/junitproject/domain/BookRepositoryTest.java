40545cb35190aa5759474f8e9daff62954489ac7
EJ <hanej93@naver.com>
2022-10-03 15:14:09 +0900

    레포지토리 테스트 - 책삭제하기 버그 수정

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -1,14 +1,16 @@
 package site.metacoding.junitproject.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 @DataJpaTest // DB와 관련된 컴포넌트만 메모리에 로딩
 public class BookRepositoryTest {
@@ -67,6 +69,7 @@ public class BookRepositoryTest {
     } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
     // 3. 책 한건보기
+    @Sql("classpath:db/tableInit.sql")
     @Test
     public void 책한건보기_test() {
         // given
@@ -81,7 +84,19 @@ public class BookRepositoryTest {
         assertEquals(author, bookPS.getAuthor());
     } // 트랜잭션 종료 (저장된 데이터를 초기화함)
 
-    // 4. 책 수정
+    // 4. 책 삭제
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책삭제_test() {
+        // given
+        Long id = 1L;
 
-    // 5. 책 삭제
+        // when
+        bookRepository.deleteById(id);
+
+        // then
+        assertFalse(bookRepository.findById(id).isPresent());
+    }
+
+    // 5. 책 수정
 }
