1c6ca0da056b8afa2e000abe9a7d16f847826b0b
EJ <hanej93@naver.com>
2022-10-03 15:30:18 +0900

    책수정_test 완료

--- src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
+++ src/test/java/site/metacoding/junitproject/domain/BookRepositoryTest.java
@@ -99,4 +99,33 @@ public class BookRepositoryTest {
     }
 
     // 5. 책 수정
+    @Sql("classpath:db/tableInit.sql")
+    @Test
+    public void 책수정_test() {
+        // given
+        Long id = 1L;
+        String title = "junit5";
+        String author = "메타코딩";
+        Book book = Book.builder()
+            .id(id)
+            .title(title)
+            .author(author)
+            .build();
+
+        // when
+        Book bookPS = bookRepository.save(book);
+
+        // bookRepository.findAll()
+        //     .forEach(b -> {
+        //         System.out.println(b.getId());
+        //         System.out.println(b.getTitle());
+        //         System.out.println(b.getAuthor());
+        //         System.out.println("==================");
+        //     });
+
+        // then
+        assertEquals(id, bookPS.getId());
+        assertEquals(title, bookPS.getTitle());
+        assertEquals(author, bookPS.getAuthor());
+    }
 }
